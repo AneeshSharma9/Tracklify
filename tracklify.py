21bbe11de5dbc4c2e82cfd23cfdef50efce255d8
Aneesh Sharma <ansh993@gmail.com>
2023-08-22 17:44:31 -0400

    getting links from each email

--- tracklify.py
+++ tracklify.py
@@ -4,6 +4,7 @@ import os.path
 import base64
 import gi
 import json
+import re
 from bs4 import BeautifulSoup
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -20,7 +21,7 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 def main():
     subjects = []
-        
+    links = []
     
     class EmailListWindow(Gtk.Window):
         def __init__(self):
@@ -38,13 +39,7 @@ def main():
             self.refresh_button.connect("clicked", self.load_emails)
             self.box.pack_start(self.refresh_button, False, False, 0)
             
-            # Create a grid layout
-            
-            #self.add(self.grid)
 
-            
-
-            
         def load_emails(self, widget):
             creds = None
             if os.path.exists('token.json'):
@@ -86,7 +81,7 @@ def main():
                     else:
                         print('Subject not found for message ID:', message['id'])
                     #print(json.dumps(msg, indent = 4))
-                    
+                    plain_text = ""
                     if 'parts' in msg['payload']:
                         plain_text_part = None
                         for part in msg['payload']['parts']:
@@ -102,7 +97,6 @@ def main():
                         else:
                             print("No plain text part found in the email.")
                     else:
-                        # Process messages without 'parts' field
                         if 'body' in msg['payload']:
                             encoded_text = msg['payload']['body']['data']
                             decoded_text = base64.urlsafe_b64decode(encoded_text).decode("utf-8")
@@ -111,25 +105,15 @@ def main():
                             print(plain_text)
                         else:
                             print("No plain text body found in the email.")
+                            
+                    def extract_links(text):
+                        url_pattern = re.compile(r'https?://\S+|www\.\S+')
+                        links = re.findall(url_pattern, text)
+                        return links
                     
-                    """
-                    #if parts in payload
-                    #if body in payload
-                    plain_text_part = None
-                    for part in msg['payload']['parts']:
-                        if part['mimeType'] == 'text/plain':
-                            plain_text_part = part
-                            break
-                    if plain_text_part:
-                        encoded_text = plain_text_part['body']['data']
-                        decoded_text = base64.urlsafe_b64decode(encoded_text).decode("utf-8")                        
-                        soup = BeautifulSoup(decoded_text, 'html.parser')
-                        plain_text = soup.get_text(separator=' ')
-                        print(plain_text)
-                    else:
-                        print("No plain text part found in the email.")
-                        """
-                    
+                    linkTemp = extract_links(plain_text)
+                    linkString = "\n".join(linkTemp)
+                    links.append(linkString)
                         
             except HttpError as error:
                 print(f'An error occurred: {error}')
@@ -142,6 +126,14 @@ def main():
                 checkbox = Gtk.CheckButton()
                 self.grid.attach(checkbox, 0, i, 1, 1)
                 self.grid.attach(label, 1, i, 1, 1)
+               
+               
+            for i, link in enumerate(links):
+                print(link)
+                linkLabel = Gtk.Label(label=str(link))
+                linkLabel.set_alignment(0, 0.5)
+                self.grid.attach(linkLabel, 2, i, 1, 1)
+                
             self.grid.show_all()
 
         def on_button_clicked(self, widget):
